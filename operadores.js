689f4cdb2fd0f8ab913e571059b8130187db796e
alessandragaleno <alessandragaleno421@gmail.com>
2023-07-21 21:29:31 -0300

    JAVASCRIPT

---
+++ operadores.js
@@ -0,0 +1,28 @@
+//aritimedicos//
+// + adição//
+// - subtração//
+// * multiplicação//
+// / divisão//
+let numeroUm = 5
+let numeroDois = 6
+let numeroTres = -1
+console.log(numeroDois + numeroTres)
+
+//Relacionais//
+//> maior que
+//< menor que
+//!= diferente
+ //   !==
+//== igual
+ //   ===
+//>= maior ou igual
+//<= menor ou igual
+
+let numeroString = '6'
+console.log(!(numeroDois === numeroString))
+// = atribuição//
+
+//Lógicos
+//&& E
+//|| OU
+//! NÃO
---
+++ variaveis.js
@@ -0,0 +1,15 @@
+console.log(nome)
+//usuario vai ou nada//
+//  Identificador//
+//      atribuiçao//
+//  valor//
+//possivel erro com undefined//
+var nome="alessandra"
+sobrenome="Galeno"
+
+//não permite a execução antes da criação (ERRO) //
+let nome2 = "Senac"
+console.log(typeof nome2)
+
+const pi=3.141923
+console.log(typeof pi)
